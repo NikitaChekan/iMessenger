bdeae160ffdff92b2a946229895620ea4d6294a6
NikitaChekan <110492748+NikitaChekan@users.noreply.github.com>
2023-04-08 17:58:06 +0300

    Initial Commit

---
+++ iMessenger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iMessenger
+//
+//  Created by jopootrivatel on 08.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
