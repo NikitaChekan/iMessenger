55686f7684fb2de1b29c764c284e78e40440cc1c
NikitaChekan <110492748+NikitaChekan@users.noreply.github.com>
2023-04-08 18:28:03 +0300

    setting project and prepating SwiftUI preview for UIKIt

--- iMessenger/ViewController.swift
+++ iMessenger/ViewController.swift
@@ -11,9 +11,32 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemPink
+        
     }
 
 
+}
+
+import SwiftUI
+
+struct ViewControllerProvider: PreviewProvider {
+    static var previews: some View {
+        ConteinerView()
+            .ignoresSafeArea()
+    }
+    
+    struct ConteinerView: UIViewControllerRepresentable {
+        
+        let viewController = ViewController()
+        
+        func makeUIViewController(context: UIViewControllerRepresentableContext<ViewControllerProvider.ConteinerView>) -> ViewController {
+            return viewController
+        }
+        
+        func updateUIViewController(_ uiViewController: ViewControllerProvider.ConteinerView.UIViewControllerType, context: UIViewControllerRepresentableContext<ViewControllerProvider.ConteinerView>) {
+            
+        }
+    }
 }
 
