4a7e2c286bd6ab288809b51270342b36f596cb7a
NikitaChekan <110492748+NikitaChekan@users.noreply.github.com>
2023-04-13 20:24:56 +0300

    hello world . create Cell group

---
+++ iMessenger/ViewControllers/Cell/ActiveChatCell.swift
@@ -0,0 +1,8 @@
+//
+//  ActiveChatCell.swift
+//  iMessenger
+//
+//  Created by jopootrivatel on 13.04.2023.
+//
+
+import Foundation
---
+++ iMessenger/ViewControllers/Cell/WaitingChatCell.swift
@@ -0,0 +1,8 @@
+//
+//  WaitingChatCell.swift
+//  iMessenger
+//
+//  Created by jopootrivatel on 13.04.2023.
+//
+
+import Foundation
