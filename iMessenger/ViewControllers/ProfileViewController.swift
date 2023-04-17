99aa2c54e7183675fc3fa11ec8473f9a7c49ac37
NikitaChekan <110492748+NikitaChekan@users.noreply.github.com>
2023-04-17 21:58:58 +0300

    create profile screen

---
+++ iMessenger/ViewControllers/ProfileViewController.swift
@@ -0,0 +1,119 @@
+//
+//  ProfileViewController.swift
+//  iMessenger
+//
+//  Created by jopootrivatel on 17.04.2023.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+    let containerView = UIView()
+    let imageView = UIImageView(image: UIImage(named: "human11"), contentMode: .scaleAspectFill)
+    let nameLabel = UILabel(
+        text: "Dasha Logina",
+        font: .systemFont(ofSize: 20, weight: .light)
+    )
+    let aboutMeLabel = UILabel(
+        text: "You have the opportunity to chat with the best girl in the world!",
+        font: .systemFont(ofSize: 16, weight: .light)
+    )
+    let myTextField = UITextField()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+//        view.backgroundColor = UIColor(named: "mainWhiteColor")
+        customiseElements()
+        setupConstraints()
+    }
+    
+    private func customiseElements() {
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        containerView.translatesAutoresizingMaskIntoConstraints = false
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        aboutMeLabel.translatesAutoresizingMaskIntoConstraints = false
+        myTextField.translatesAutoresizingMaskIntoConstraints = false
+        
+        aboutMeLabel.numberOfLines = 0
+        
+        containerView.backgroundColor = UIColor(named: "mainWhiteColor")
+        containerView.layer.cornerRadius = 30
+        
+        myTextField.borderStyle = .roundedRect
+        myTextField.placeholder = "Print you'r message"
+    }
+    
+}
+
+extension ProfileViewController {
+    
+    private func setupConstraints() {
+        
+        view.addSubview(imageView)
+        view.addSubview(containerView)
+        containerView.addSubview(nameLabel)
+        containerView.addSubview(aboutMeLabel)
+        containerView.addSubview(myTextField)
+        
+        NSLayoutConstraint.activate([
+            imageView.topAnchor.constraint(equalTo: view.topAnchor),
+            imageView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            imageView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            imageView.bottomAnchor.constraint(equalTo: containerView.topAnchor, constant: 30)
+        ])
+        
+        NSLayoutConstraint.activate([
+            containerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            containerView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            containerView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            containerView.heightAnchor.constraint(equalToConstant: 206)
+        ])
+        
+        NSLayoutConstraint.activate([
+            nameLabel.topAnchor.constraint(equalTo: containerView.topAnchor, constant: 35),
+            nameLabel.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 24),
+            nameLabel.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -24)
+        ])
+        
+        NSLayoutConstraint.activate([
+            aboutMeLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 8),
+            aboutMeLabel.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 24),
+            aboutMeLabel.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -24)
+        ])
+        
+        NSLayoutConstraint.activate([
+            myTextField.topAnchor.constraint(equalTo: aboutMeLabel.bottomAnchor, constant: 8),
+            myTextField.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 24),
+            myTextField.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -24),
+            myTextField.heightAnchor.constraint(equalToConstant: 48)
+        ])
+        
+    }
+}
+
+// MARK: - SwiftUI
+
+import SwiftUI
+
+struct ProfileVCProvider: PreviewProvider {
+    static var previews: some View {
+        ConteinerView()
+            .ignoresSafeArea()
+    }
+    
+    struct ConteinerView: UIViewControllerRepresentable {
+        
+        let profileVC = ProfileViewController()
+        
+        func makeUIViewController(context: UIViewControllerRepresentableContext<ProfileVCProvider.ConteinerView>) -> ProfileViewController {
+            return profileVC
+        }
+        
+        func updateUIViewController(_ uiViewController: ProfileVCProvider.ConteinerView.UIViewControllerType, context: UIViewControllerRepresentableContext<ProfileVCProvider.ConteinerView>) {
+            
+        }
+    }
+}
+
