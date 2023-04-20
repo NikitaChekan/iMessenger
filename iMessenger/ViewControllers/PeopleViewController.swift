872e938bc138d16b369e4460135612130dfa09b3
NikitaChekan <110492748+NikitaChekan@users.noreply.github.com>
2023-04-21 00:05:35 +0300

    fixed deprecate log out functional

--- iMessenger/ViewControllers/PeopleViewController.swift
+++ iMessenger/ViewControllers/PeopleViewController.swift
@@ -45,7 +45,16 @@ class PeopleViewController: UIViewController {
         alertController.addAction(UIAlertAction(title: "Sign Out", style: .destructive, handler: { (_) in
             do {
                 try Auth.auth().signOut()
-                UIApplication.shared.keyWindow?.rootViewController = AuthViewController()
+//                UIApplication.shared.keyWindow?.rootViewController = AuthViewController()
+                let keyWindow = UIApplication.shared.connectedScenes
+                    .filter({$0.activationState == .foregroundActive})
+                    .map({$0 as? UIWindowScene})
+                    .compactMap({$0})
+                    .first?.windows
+                    .filter({$0.isKeyWindow}).first
+                
+                keyWindow?.rootViewController = AuthViewController()
+                
             } catch {
                 print("Error signing out: \(error.localizedDescription)")
             }
